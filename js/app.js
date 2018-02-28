9619f234719232008e1fb939be4c9c8c01fc4ec6
RONQUILLO <jr9099@dt2017-dfwrtr-002-tlbx.ks.us.att.com>
2018-02-27 16:11:24 -0800

    add test function in app.js for simulating json data from a server

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
-function AppViewModel() {
-	this.title = "title!";
+// Class to represent a row in the seat reservations grid
+function DataClass(name, name2) {
+    var self = this;
+    self.name = name;
+    self.title = ko.observable(name2);
+
+    
 }
 
+// Overall viewmodel for this screen, along with initial state
+function dataViewModel() {
+    var self = this;
 
-ko.applyBindings(new AppViewModel());
+    // Non-editable catalog data - would come from the server
+    self.jsonDataFromServer = [
+        { title: "test_title_1" },
+    ];    
+
+    console.log(self.jsonDataFromServer[0]);
+    // Editable data
+    self.chosenData = ko.observableArray([
+        new DataClass("Steve", self.jsonDataFromServer[0]),
+    ]);
+}
+
+ko.applyBindings(new dataViewModel());
