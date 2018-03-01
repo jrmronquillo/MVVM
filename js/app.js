198c431c418ae0a10ecda370dad4412dede7bb9b
RONQUILLO <jr9099@ELSML03JR909A-3.local>
2018-03-01 15:14:22 -0800

    add data retrieval from a JSON source

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 // Class to represent a row in the seat reservations grid
 function DataClass(name, name2) {
     var self = this;
@@ -7,20 +9,32 @@ function DataClass(name, name2) {
     
 }
 
+// Note: that the getJSON wraps the entire javascript body
+ $.getJSON("js/dataList.json", function( jsonData ){
+        const DataClass2 = function(data){
+            this.name = ko.observable(data.title);
+        };
+
 // Overall viewmodel for this screen, along with initial state
 function dataViewModel() {
     var self = this;
+
+    console.log(jsonData.dataList);
+
+    this.appError = ko.observable();  
 
     // Non-editable catalog data - would come from the server
     self.jsonDataFromServer = [
         { title: "test_title_1" },
     ];    
 
-    console.log(self.jsonDataFromServer[0]);
+    // console.log(self.jsonDataFromServer[0]);
     // Editable data
     self.chosenData = ko.observableArray([
         new DataClass("Steve", self.jsonDataFromServer[0]),
     ]);
-}
-
+    
+    }
 ko.applyBindings(new dataViewModel());
+
+});
